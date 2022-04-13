6cfed7a68b4d2c4efdbb387bc5c735a8cdfac516
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-04-13 17:33:23 -0500

    Made some css changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Weather from "./Weather.js";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Weather />
+      <footer className="reference">
+        <a
+          href="https://github.com/paulinagonzalezc/weather-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>
+        <span> by Paulina Gonzalez</span>
+      </footer>
     </div>
   );
 }
