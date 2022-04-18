63af9066ef80f2f4bcf1c49a3f1501e6c7efc637
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-04-17 20:04:04 -0500

    Mover API request info to function

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,6 +16,14 @@ export default function WeatherForecast(props) {
     setLoaded(true);
   }
 
+  function load() {
+    let apiKey = "91ddd16622a3322fcdf9394f7672a655";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (loaded) {
     return (
       <div className="row WeatherForecast">
@@ -33,11 +41,6 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiKey = "91ddd16622a3322fcdf9394f7672a655";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-    return null;
+    load();
   }
 }
